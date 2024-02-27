170de19e0f86bad8a15e45ae3b9aef2aa848fb19
Manuel Esteban <m.estebanespinosa@edu.gva.es>
2024-02-27 05:54:58 +0100

    multiplicacion y division

--- operations.js
+++ operations.js
@@ -8,9 +8,26 @@ var resta = function suma (a, b) {
 	return a - b;	
 };
 
+
+/* Función multiplicacion */
+
+var multiplicacion = function multiplicacion(a, b) {
+  return a * b;
+}
+
+
+/* Función division */
+
+var division = function division (a, b) {
+  return a / b;
+}
+
+
 /* Exportación de funciones */
 module.exports = {
 	suma,
-	resta
+	resta,
+	multiplicacion,
+	division
 }
 
